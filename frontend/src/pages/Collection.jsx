8d620dfeced7e1ab4914206bf1ff165315329a68
tanya-pvt2005 <tanya.pvt2005@gmail.com>
2025-10-24 14:07:28 +0530

    Collections

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
-
+import {ShopContext} from "../context/ShopContext"
+import { useContext, useState} from 'react';
 const Collection = () => {
+
+  //first we need data of all products
+  const {products} = useContext(ShopContext);
+  const [showFilter, setShowFilter] = useState(false)
+
   return (
-    <div>
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
+
+      {/* filter options */}
+      <div className = 'min-w-60'>
+        <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS</p>
+        {/* category filters */}
+        <div className={`border border-gray-300 pl-5 py-3 mt-6  ${showFilter ? '' : 'hidden'  }`}>
+          <p className = 'mb-3 text-sm font-medium'>CATEGORIES</p>
+          <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+            <p className='flex gap-2'>
+              <input type= 'checkbox' className='w-3' value = {'Men'}/>Men
+            </p>
+             <p className='flex gap-2'>
+              <input type= 'checkbox' className='w-3' value = {'Women'}/>Women
+            </p>
+             <p className='flex gap-2'>
+              <input type= 'checkbox' className='w-3' value = {'Kids'}/>Kids
+            </p>
+          </div>
+          
+        </div>
+      </div>
       
     </div>
   )
